968011b25e85b20541addb431d088e1950b39a8e
An Tran <vuan.tran@zuhlke.com>
2021-07-19 20:52:15 +0700

    model, and modelnetworkentity, update serialize plugin in settings.gradle

---
+++ app/src/main/java/com/example/jetpackmvvm/domain/model/Recipe.kt
@@ -0,0 +1,19 @@
+package com.example.jetpackmvvm.domain.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Recipe(
+    val id: Int? = null,
+    val title: String? = null,
+    val publisher: String? = null,
+    val featuredImage: String? = null,
+    val rating: Int? = null,
+    val sourceUrl: String? = null,
+    val description: String? = null,
+    val cookingInstructions: String? = null,
+    val ingredients: List<String> = listOf(),
+    val dateAdded: String? = null,
+    val dateUpdated: String? = null,
+) : Parcelable
---
+++ app/src/main/java/com/example/jetpackmvvm/network/model/RecipeNetworkEntity.kt
@@ -0,0 +1,35 @@
+package com.example.jetpackmvvm.network.model
+
+import com.google.gson.annotations.SerializedName
+
+class RecipeNetworkEntity(
+    @SerializedName("pk")
+    var pk: Int? = null,
+
+    @SerializedName("title")
+    var title: String? = null,
+
+    @SerializedName("publisher")
+    var publisher: String? = null,
+
+    @SerializedName("rating")
+    var rating: Int? = null,
+
+    @SerializedName("source_url")
+    var sourceUrl: String? = null,
+
+    @SerializedName("description")
+    var description: String? = null,
+
+    @SerializedName("cooking_instructions")
+    var cookingInstructions: String? = null,
+
+    @SerializedName("ingredients")
+    var ingredients: List<String>? = null,
+
+    @SerializedName("date_added")
+    var dateAdded: String? = null,
+
+    @SerializedName("date_updated")
+    var dateUpdated: String? = null,
+)
