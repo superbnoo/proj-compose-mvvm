2c557388d40173d780ee39cc75b654db3803501a
An Tran <vuan.tran@zuhlke.com>
2021-07-19 16:23:14 +0700

    column

--- app/src/main/java/com/example/jetpackmvvm/MainActivity.kt
+++ app/src/main/java/com/example/jetpackmvvm/MainActivity.kt
@@ -5,17 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackmvvm.ui.theme.JetpackMVVMTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,8 +41,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainContent() {
+    val scrollState = rememberScrollState()
+
     Column(
-        modifier = Modifier.padding(16.dp)
+        modifier = Modifier
+            .padding(16.dp)
+            .verticalScroll(scrollState)
     ) {
         Image(
             painter = painterResource(id = R.drawable.happy_meal_small),
@@ -43,6 +54,35 @@ fun MainContent() {
             modifier = Modifier.height(300.dp),
             contentScale = ContentScale.Crop,
         )
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "Happy Meal",
+                style = TextStyle(
+                    fontSize = 30.sp
+                )
+            )
+            Spacer(
+                modifier = Modifier.padding(top = 10.dp)
+            )
+            Text(
+                text = "800 Calories",
+                style = TextStyle(
+                    fontSize = 17.sp
+                )
+            )
+            Spacer(
+                modifier = Modifier.padding(top = 10.dp)
+            )
+            Text(
+                text = "$5.99",
+                style = TextStyle(
+                    color = Color(0xFF85bb65),
+                    fontSize = 17.sp
+                )
+            )
+        }
     }
 }
 
