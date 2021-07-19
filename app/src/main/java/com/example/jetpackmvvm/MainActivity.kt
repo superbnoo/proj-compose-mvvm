2832479836284b4b3ece0efdcd2ab3b5e0bd8430
An Tran <vuan.tran@zuhlke.com>
2021-07-19 16:38:00 +0700

    happy meal order now

--- app/src/main/java/com/example/jetpackmvvm/MainActivity.kt
+++ app/src/main/java/com/example/jetpackmvvm/MainActivity.kt
@@ -4,23 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackmvvm.ui.theme.JetpackMVVMTheme
@@ -57,12 +55,25 @@ fun MainContent() {
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
-            Text(
-                text = "Happy Meal",
-                style = TextStyle(
-                    fontSize = 30.sp
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "Happy Meal",
+                    style = TextStyle(
+                        fontSize = 30.sp
+                    )
                 )
-            )
+                Text(
+                    text = "$5.99",
+                    style = TextStyle(
+                        color = Color(0xFF85bb65),
+                        fontSize = 17.sp
+                    ),
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
             Spacer(
                 modifier = Modifier.padding(top = 10.dp)
             )
@@ -75,13 +86,15 @@ fun MainContent() {
             Spacer(
                 modifier = Modifier.padding(top = 10.dp)
             )
-            Text(
-                text = "$5.99",
-                style = TextStyle(
-                    color = Color(0xFF85bb65),
-                    fontSize = 17.sp
+            Button(
+                onClick = {},
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = "ORDER NOW"
                 )
-            )
+            }
+
         }
     }
 }
