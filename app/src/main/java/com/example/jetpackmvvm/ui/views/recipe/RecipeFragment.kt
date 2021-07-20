88a533e531d7ade7aaa8e40fbaad7dcc105d688a
An Tran <vuan.tran@zuhlke.com>
2021-07-20 09:55:39 +0700

    activityViewModels to share vm among fragments

--- app/src/main/java/com/example/jetpackmvvm/ui/views/recipe/RecipeFragment.kt
+++ app/src/main/java/com/example/jetpackmvvm/ui/views/recipe/RecipeFragment.kt
@@ -1,49 +1,48 @@
-package com.example.jetpackmvvm.ui.views
+package com.example.jetpackmvvm.ui.views.recipe
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import com.example.jetpackmvvm.R
+import androidx.fragment.app.activityViewModels
+import com.example.jetpackmvvm.ui.views.recipe_list.RecipeListViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class RecipeListFragment: Fragment() {
+@AndroidEntryPoint
+class RecipeFragment: Fragment() {
+
+    val viewModel: RecipeListViewModel by activityViewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println("RecipeFragment $viewModel")
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return ComposeView(requireContext()).apply {
             setContent {
-                Column(modifier = Modifier.padding(16.dp)) {
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
                     Text(
-                        text = "Recipe List",
+                        text = "RECIPE FRAGMENT",
                         style = TextStyle(
                             fontSize = 21.sp
                         )
                     )
-                    Spacer(modifier = Modifier.padding(10.dp))
-                    Button(onClick = {
-                        findNavController().navigate(R.id.viewRecipe)
-                    }) {
-                        Text(text = "TO RECIPE FRAGMENT")
-                    }
                 }
             }
         }
---
+++ app/src/main/java/com/example/jetpackmvvm/ui/views/recipe_list/RecipeListFragment.kt
@@ -0,0 +1,59 @@
+package com.example.jetpackmvvm.ui.views.recipe_list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import com.example.jetpackmvvm.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RecipeListFragment: Fragment() {
+
+    val viewModel: RecipeListViewModel by activityViewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println("RecipeListFragment $viewModel")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                        text = "Recipe List",
+                        style = TextStyle(
+                            fontSize = 21.sp
+                        )
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Button(onClick = {
+                        findNavController().navigate(R.id.viewRecipe)
+                    }) {
+                        Text(text = "TO RECIPE FRAGMENT")
+                    }
+                }
+            }
+        }
+    }
+}
