d0aaa881e860a099c9b2df87f5a2d0b2d6762a8f
An Tran <vuan.tran@zuhlke.com>
2021-07-20 15:05:17 +0700

    remember - similar to useState in react

--- app/src/main/java/com/example/jetpackmvvm/ui/views/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/example/jetpackmvvm/ui/views/recipe_list/RecipeListFragment.kt
@@ -1,7 +1,6 @@
 package com.example.jetpackmvvm.ui.views.recipe_list
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,25 +9,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.findNavController
-import com.example.jetpackmvvm.R
 import com.example.jetpackmvvm.ui.components.RecipeCard
-import com.example.jetpackmvvm.util.TAG
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RecipeListFragment: Fragment() {
 
-    val viewModel: RecipeListViewModel by viewModels()
+    private val viewModel: RecipeListViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,14 +33,23 @@ class RecipeListFragment: Fragment() {
 
         return ComposeView(requireContext()).apply {
             setContent {
-
                 val recipes = viewModel.recipes.value
+                val (query, setQuery) = remember { mutableStateOf("beef") }
 
-                LazyColumn {
-                    itemsIndexed(
-                      items = recipes
-                    ) { index, recipe ->
-                        RecipeCard(recipe = recipe, onClick = {})
+                Column {
+                    TextField(
+                        value = query,
+                        onValueChange = {
+                            setQuery(it)
+                         }
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    LazyColumn {
+                        itemsIndexed(
+                            items = recipes
+                        ) { index, recipe ->
+                            RecipeCard(recipe = recipe, onClick = {})
+                        }
                     }
                 }
             }
